b44f1a74bd905e1463dd63b2132e59f07f37b4c0
dkbarrett <dk.barrett+gh@hotmail.com>
2021-08-01 20:41:30 +1200

    Refactoring, increase readability

--- components/webserver/site/src/script.js
+++ components/webserver/site/src/script.js
@@ -1,54 +1,70 @@
-var AttackStateEnum = { READY: 0, RUNNING: 1, FINISHED: 2, TIMEOUT: 3 };
-var AttackTypeEnum = { ATTACK_TYPE_PASSIVE: 0, ATTACK_TYPE_HANDSHAKE: 1, ATTACK_TYPE_PMKID: 2, ATTACK_TYPE_DOS: 3 };
-var selectedApElement = -1;
+const AttackStateEnum = { READY: 0, RUNNING: 1, FINISHED: 2, TIMEOUT: 3 };
+const AttackTypeEnum = { ATTACK_TYPE_PASSIVE: 0, ATTACK_TYPE_HANDSHAKE: 1, ATTACK_TYPE_PMKID: 2, ATTACK_TYPE_DOS: 3 };
+const RUNNING_POLL_INTERVAL = 1000;
+const POLL_INTERVAL = 1000;
+const ESP_URL = "http://192.168.4.1"
+
+var selectedApElement = null;
 var poll;
-var poll_interval = 1000;
-var running_poll;
-var running_poll_interval = 1000;
-var attack_timeout = 0;
-var time_elapsed = 0;
-var defaultResultContent = $("result").innerHTML;
+var runningPoll;
+var attackTimeout = 0;
+var timeElapsed = 0;
+var defaultResultContent = $("results").innerHTML;
 var defaultAttackMethods = $("attack_method").outerHTML;
 
 function $(id) {
     return document.getElementById(id)
 }
 
-function showError(text) {
-    $('errors').innerText = text;
-    $('errors').style.display = "block";
+function updateError(text) {
+    if (text) {
+        $('errors').innerText = text;
+        $('errors').classList.remove("hidden");
+        $('errors').classList.add("visible");
+    } else {
+        $('errors').classList.add("hidden");
+        $('errors').classList.remove("visible");
+    }
 }
 
-function hideError() {
-    $('errors').style.display = "none";
+function showSection(sectionId) {
+    if ($(sectionId).classList.contains("hidden")) {
+        for (let section of document.getElementsByTagName("section")) {
+            section.classList.remove("visible");
+            section.classList.add("hidden");
+        };
+        $(sectionId).classList.remove("hidden");
+        $(sectionId).classList.add("visible");
+    }
 }
 
 function getStatus() {
-    hideError();
-    var oReq = new XMLHttpRequest();
-    oReq.onload = function () {
-        var arrayBuffer = oReq.response;
+    updateError(null);
+    var statusRequest = new XMLHttpRequest();
+    statusRequest.onload = function () {
+        var arrayBuffer = statusRequest.response;
         if (arrayBuffer) {
-            var attack_state = parseInt(new Uint8Array(arrayBuffer, 0, 1));
-            var attack_type = parseInt(new Uint8Array(arrayBuffer, 1, 1));
-            var attack_content_size = parseInt(new Uint16Array(arrayBuffer, 2, 1));
-            var attack_content = new Uint8Array(arrayBuffer, 4);
-            console.log("attack_state=" + attack_state + "; attack_type=" + attack_type + "; attack_count_size=" + attack_content_size);
-            hideAllSections();
-            switch (attack_state) {
+            var attackState = parseInt(new Uint8Array(arrayBuffer, 0, 1));
+            var attackType = parseInt(new Uint8Array(arrayBuffer, 1, 1));
+            var attackContentSize = parseInt(new Uint16Array(arrayBuffer, 2, 1));
+            var attackContent = new Uint8Array(arrayBuffer, 4);
+            console.log("attack_state=" + attackState + "; attack_type=" + attackType + "; attack_count_size=" + attackContentSize);
+            switch (attackState) {
                 case AttackStateEnum.READY:
-                    showAttackConfig();
+                    fetchAps();
+                    showSection("setup");
                     break;
                 case AttackStateEnum.RUNNING:
-                    showRunning();
-                    console.log("Poll");
-                    setTimeout(getStatus, poll_interval);
+                    showSection("running")
+                    setTimeout(getStatus, RUNNING_POLL_INTERVAL);
                     break;
                 case AttackStateEnum.FINISHED:
-                    showResult("FINISHED", attack_type, attack_content_size, attack_content);
+                    showResult("FINISHED", attackType, attackContentSize, attackContent);
+                    showSection("results")
                     break;
                 case AttackStateEnum.TIMEOUT:
-                    showResult("TIMEOUT", attack_type, attack_content_size, attack_content);
+                    showResult("TIMEOUT", attackType, attackContentSize, attackContent);
+                    showSection("results")
                     break;
                 default:
                     $("errors").innerHTML = "Error loading attack status! Unknown state.";
@@ -56,58 +72,47 @@ function getStatus() {
             return;
         }
     };
-    oReq.onerror = function () {
-        showError("Cannot reach ESP32. Check that you are connected to management AP. You might get disconnected during attack.");
+    statusRequest.onerror = function () {
+        updateError("Cannot reach ESP32. Check that you are connected to management AP. You might get disconnected during attack.");
         getStatus();
     };
-    oReq.ontimeout = function () {
+    statusRequest.ontimeout = function () {
         console.log("Request timeout");
         getStatus();
     };
-    oReq.open("GET", "http://192.168.4.1/status", true);
-    oReq.responseType = "arraybuffer";
-    oReq.send();
+    statusRequest.open("GET", ESP_URL + "/status", true);
+    statusRequest.responseType = "arraybuffer";
+    statusRequest.send();
 }
-function hideAllSections() {
-    for (let section of document.getElementsByTagName("section")) {
-        section.style.display = "none";
-    };
-}
-function showRunning() {
-    hideAllSections();
-    $("running").style.display = "block";
-}
+
+
 function countProgress() {
-    if (time_elapsed >= attack_timeout) {
+    if (timeElapsed >= attackTimeout) {
         $("errors").innerHTML = "Please reconnect to management AP";
         $("errors").style.display = "block";
-        clearInterval(running_poll);
+        clearInterval(runningPoll);
     }
-    $("running-progress").innerHTML = time_elapsed + "/" + attack_timeout + "s";
-    time_elapsed++;
+    $("running-progress").innerHTML = timeElapsed + "/" + attackTimeout + "s";
+    timeElapsed++;
 }
-function showAttackConfig() {
-    $("ready").style.display = "block";
-    refreshAps();
-}
-function showResult(status, attack_type, attack_content_size, attack_content) {
-    hideAllSections();
+
+
+function showResult(status, attackType, attackContentSize, attackContent) {
     clearInterval(poll);
-    $("result").innerHTML = defaultResultContent;
-    $("result").style.display = "block";
+    $("results").innerHTML = defaultResultContent;
     $("result-meta").innerHTML = status + "<br>";
     type = "ERROR: Cannot parse attack type.";
-    switch (attack_type) {
+    switch (attackType) {
         case AttackTypeEnum.ATTACK_TYPE_PASSIVE:
             type = "ATTACK_TYPE_PASSIVE";
             break;
         case AttackTypeEnum.ATTACK_TYPE_HANDSHAKE:
             type = "ATTACK_TYPE_HANDSHAKE";
-            resultHandshake(attack_content, attack_content_size);
+            resultHandshake(attackContent, attackContentSize);
             break;
         case AttackTypeEnum.ATTACK_TYPE_PMKID:
             type = "ATTACK_TYPE_PMKID";
-            resultPmkid(attack_content, attack_content_size);
+            resultPmkid(attackContent, attackContentSize);
             break;
         case AttackTypeEnum.ATTACK_TYPE_DOS:
             type = "ATTACK_TYPE_DOS";
@@ -118,6 +123,7 @@ function showResult(status, attack_type, attack_content_size, attack_content) {
     $("result-meta").innerHTML += type + "<br>";
 }
 
+
 function generateRow(rowId, ssid, bssid, rssi) {
     tr = document.createElement('tr');
     tr.setAttribute("id", rowId);
@@ -126,14 +132,18 @@ function generateRow(rowId, ssid, bssid, rssi) {
     return tr;
 }
 
-function refreshAps() {
-    hideError();
+
+function fetchAps() {
+    updateError(null);
+    selectedApElement = null;
+    $("ap-loader").style.display = "block";
     let apList = $("ap-list");
-    apList.innerHTML = "Loading (this may take a while)...";
-    var oReq = new XMLHttpRequest();
-    oReq.onload = function () {
+    apList.innerHTML = "";
+    var apRequest = new XMLHttpRequest();
+    apRequest.onload = function () {
+        $("ap-loader").style.display = "none";
         apList.innerHTML = "<th>SSID</th><th>BSSID</th><th>RSSI</th>";
-        var arrayBuffer = oReq.response;
+        var arrayBuffer = apRequest.response;
         if (arrayBuffer) {
             var byteArray = new Uint8Array(arrayBuffer);
             for (let i = 0; i < byteArray.byteLength; i = i + 40) {
@@ -152,30 +162,31 @@ function refreshAps() {
             }
         }
     };
-    oReq.onerror = function () {
+    apRequest.onerror = function () {
+        $("ap-loader").style.display = "none";
         apList.innerHTML = "ERROR";
-        showError('Unable to load AP list')
+        updateError('Unable to load AP list')
     };
-    oReq.open("GET", "http://192.168.4.1/ap-list", true);
-    oReq.responseType = "arraybuffer";
-    oReq.send();
+    apRequest.open("GET", ESP_URL + "/ap-list", true);
+    apRequest.responseType = "arraybuffer";
+    apRequest.send();
 }
+
+
 function selectAp(el) {
-    console.log(el.id);
-    if (selectedApElement != -1) {
+    if (selectedApElement != null) {
         selectedApElement.classList.remove("selected")
     }
     selectedApElement = el;
     el.classList.add("selected");
 }
+
+
 function runAttack() {
-    if (selectedApElement == -1) {
-        console.log("No AP selected. Attack not started.");
-        $("errors").innerHTML = "No AP selected. Attack not started.";
+    if (selectedApElement == null) {
+        updateError("Attack not started. No AP selected.");
         return;
     }
-    hideAllSections();
-    $("running").style.display = "block";
     var arrayBuffer = new ArrayBuffer(4);
     var uint8Array = new Uint8Array(arrayBuffer);
     uint8Array[0] = parseInt(selectedApElement.id);
@@ -183,79 +194,91 @@ function runAttack() {
     uint8Array[2] = parseInt($("attack_method").value);
     uint8Array[3] = parseInt($("attack_timeout").value);
     var oReq = new XMLHttpRequest();
-    oReq.open("POST", "http://192.168.4.1/run-attack", true);
+    oReq.open("POST", ESP_URL + "/run-attack", true);
     oReq.send(arrayBuffer);
+
     getStatus();
-    attack_timeout = parseInt($("attack_timeout").value);
-    time_elapsed = 0;
-    running_poll = setInterval(countProgress, running_poll_interval);
+
+    attackTimeout = parseInt($("attack_timeout").value);
+    timeElapsed = 0;
+    runningPoll = setInterval(countProgress, RUNNING_POLL_INTERVAL);
 }
+
+
 function resetAttack() {
-    hideAllSections();
-    showAttackConfig();
-    var oReq = new XMLHttpRequest();
-    oReq.open("HEAD", "http://192.168.4.1/reset", true);
-    oReq.send();
+    showSection("setup");
+    fetchAps();
+    var resetRequest = new XMLHttpRequest();
+    resetRequest.open("HEAD", ESP_URL + "/reset", true);
+    resetRequest.send();
 }
-function resultPmkid(attack_content, attack_content_size) {
-    var mac_ap = "";
-    var mac_sta = "";
+
+
+function resultPmkid(attackContent, attackContentSize) {
+    var macAp = "";
+    var macSta = "";
     var ssid = "";
-    var ssid_text = "";
+    var ssidText = "";
     var pmkid = "";
     var index = 0;
     for (let i = 0; i < 6; i = i + 1) {
-        mac_ap += uint8ToHex(attack_content[index + i]);
+        macAp += uint8ToHex(attackContent[index + i]);
     }
-    index = index + 6;
+    index += 6;
     for (let i = 0; i < 6; i = i + 1) {
-        mac_sta += uint8ToHex(attack_content[index + i]);
+        macSta += uint8ToHex(attackContent[index + i]);
     }
-    index = index + 6;
-    for (let i = 0; i < attack_content[index]; i = i + 1) {
-        ssid += uint8ToHex(attack_content[index + 1 + i]);
-        ssid_text += String.fromCharCode(attack_content[index + 1 + i]);
+    index += 6;
+    for (let i = 0; i < attackContent[index]; i = i + 1) {
+        ssid += uint8ToHex(attackContent[index + 1 + i]);
+        ssidText += String.fromCharCode(attackContent[index + 1 + i]);
     }
-    index = index + attack_content[index] + 1;
-    var pmkid_cnt = 0;
-    for (let i = 0; i < attack_content_size - index; i = i + 1) {
+    index += attackContent[index] + 1;
+    var pmkidCount = 0;
+    for (let i = 0; i < attackContentSize - index; i = i + 1) {
         if ((i % 16) == 0) {
             pmkid += "<br>";
-            pmkid += "</code>PMKID #" + pmkid_cnt + ": <code>";
-            pmkid_cnt += 1;
+            pmkid += "</code>PMKID #" + pmkidCount + ": <code>";
+            pmkidCount += 1;
         }
-        pmkid += uint8ToHex(attack_content[index + i]);
+        pmkid += uint8ToHex(attackContent[index + i]);
     }
     $("result-content").innerHTML = "";
-    $("result-content").innerHTML += "MAC AP: <code>" + mac_ap + "</code><br>";
-    $("result-content").innerHTML += "MAC STA: <code>" + mac_sta + "</code><br>";
-    $("result-content").innerHTML += "(E)SSID: <code>" + ssid + "</code> (" + ssid_text + ")";
+    $("result-content").innerHTML += "MAC AP: <code>" + macAp + "</code><br>";
+    $("result-content").innerHTML += "MAC STA: <code>" + macSta + "</code><br>";
+    $("result-content").innerHTML += "(E)SSID: <code>" + ssid + "</code> (" + ssidText + ")";
     $("result-content").innerHTML += "<code>" + pmkid + "</code><br>";
     $("result-content").innerHTML += "<br>Hashcat ready format:"
-    $("result-content").innerHTML += "<code>" + pmkid + "*" + mac_ap + "*" + mac_sta + "*" + ssid + "</code><br>";
+    $("result-content").innerHTML += "<code>" + pmkid + "*" + macAp + "*" + macSta + "*" + ssid + "</code><br>";
 }
-function resultHandshake(attack_content, attack_content_size) {
+
+
+function resultHandshake(attackContent, attackContentSize) {
     $("result-content").innerHTML = "";
-    var pcap_link = document.createElement("a");
-    pcap_link.setAttribute("href", "capture.pcap");
-    pcap_link.text = "Download PCAP file";
-    var hccapx_link = document.createElement("a");
-    hccapx_link.setAttribute("href", "capture.hccapx");
-    hccapx_link.text = "Download HCCAPX file";
-    $("result-content").innerHTML += "<p>" + pcap_link.outerHTML + "</p>";
-    $("result-content").innerHTML += "<p>" + hccapx_link.outerHTML + "</p>";
+    var pcapLink = document.createElement("a");
+    pcapLink.setAttribute("href", "capture.pcap");
+    pcapLink.text = "Download PCAP file";
+    var hccapxLink = document.createElement("a");
+    hccapxLink.setAttribute("href", "capture.hccapx");
+    hccapxLink.text = "Download HCCAPX file";
+    $("result-content").innerHTML += "<p>" + pcapLink.outerHTML + "</p>";
+    $("result-content").innerHTML += "<p>" + hccapxLink.outerHTML + "</p>";
     var handshakes = "";
-    for (let i = 0; i < attack_content_size; i = i + 1) {
-        handshakes += uint8ToHex(attack_content[i]);
+    for (let i = 0; i < attackContentSize; i = i + 1) {
+        handshakes += uint8ToHex(attackContent[i]);
         if (i % 50 == 49) {
             handshakes += "\n";
         }
     }
     $("result-content").innerHTML += "<pre><code>" + handshakes + "</code></pre>";
 }
+
+
 function uint8ToHex(uint8) {
     return ("00" + uint8.toString(16)).slice(-2);
 }
+
+
 function updateConfigurableFields(el) {
     $("attack_method").outerHTML = defaultAttackMethods;
     switch (parseInt(el.value)) {
@@ -281,6 +304,8 @@ function updateConfigurableFields(el) {
             break;
     }
 }
+
+
 function setAttackMethods(attackMethodsArray) {
     $("attack_method").removeAttribute("disabled");
     attackMethodsArray.forEach(function (method, index) {
